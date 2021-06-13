991ad99d024123a484583407ada78f8bffd8ed41
Winter4 <Crowl1ngNice@yandex.ru>
2021-06-13 11:01:30 +0200

    Init

---
+++ Source.cpp
@@ -0,0 +1,13 @@
+#include <SFML/Graphics.hpp>
+
+int main()
+{
+	sf::RenderWindow window(sf::VideoMode(800, 600), "Title");
+	while (window.isOpen()) {
+		sf::Event event;
+		while (window.pollEvent(event)) {
+			if (event.type == sf::Event::Closed)
+				window.close();
+		}
+	}
+}
